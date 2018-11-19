24a4cfcae35618e8a3698847adce244e824b50b3
Jinw <jinweihz@163.com>
2018-11-20 00:24:27 +0800

    Basic function for Nlog

---
+++ Nlog/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nlog;
+using NLog;
+using System.Threading;
+
+namespace Nlog
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+            for(; ; )
+            {
+                logger.Log(LogLevel.Trace, "This is NO.{0} log message", a++);
+                logger.Log(LogLevel.Debug, "This is NO.{0} log message", a++);
+                logger.Log(LogLevel.Info, "This is NO.{0} log message", a++);
+                logger.Log(LogLevel.Error, "This is NO.{0} log message", a++);
+                logger.Log(LogLevel.Fatal, "This is NO.{0} log message", a++);
+                logger.Log(LogLevel.Warn, "This is NO.{0} log message", a++);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
